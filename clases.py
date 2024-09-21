c38a2ec4a93609602c241bacb762526c51a28286
Chaval044270 <3033373110108@ingenieria.usac.edu.gt>
2024-09-20 21:01:20 -0600

    tercer commit

--- clases.py
+++ clases.py
@@ -1,10 +1,29 @@
 class Auto:
+    autos = []
 
-    def __init__(self, idTipo, marca, modelo, descripcion, precioUnitario, cantidad, imagen):
-        self.idTipo = idTipo
+    def __init__(self, tipo, marca, modelo, descripcion, precio, cantidad, urlImagen):
+        self.tipo = tipo
         self.marca = marca
         self.modelo = modelo
         self.descripcion = descripcion
-        self.precioUnitario = precioUnitario
+        self.precio = precio
         self.cantidad = cantidad
-        self.imagen = imagen
+        self.urlImagen = urlImagen
+
+    def __str__(self):
+        return f"Tipo: {self.tipo}, Marca: {self.marca}, Modelo: {self.modelo}, Descripción: {self.descripcion}, Precio: {self.precio}, Cantidad: {self.cantidad}, URL Imagen: {self.urlImagen}"
+
+    @classmethod
+    def agregarAuto(cls, tipo, marca, modelo, descripcion, precio, cantidad, urlImagen):
+        cls.autos.append(Auto(tipo, marca, modelo, descripcion, precio, cantidad, urlImagen))
+        return cls.autos
+
+    @classmethod
+    def eliminarAuto(cls, idTipo_eliminar):
+        cls.autos = [auto for auto in cls.autos if auto.tipo != idTipo_eliminar]
+
+    @classmethod
+    def obtenerAutos(cls):
+        return cls.autos
+    
+
--- main.py
+++ main.py
@@ -1,74 +1,75 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from clases import Auto
 
-class MyApp:
-    def __init__(self):
-        self.app = Flask(__name__)
-        self.app.secret_key = 'clave_secreta'
-        self.setup_routes()
+app = Flask(__name__)
 
-    def setup_routes(self):
-        @self.app.route('/', methods=['GET', 'POST'])
-        def index():
-            error = None
-            if 'error' in session:
-                error = session['error']
-                session.pop('error', None)
 
-            if request.method == 'POST':
-                username = request.form.get('username')
-                password = request.form.get('password')
-                if username == 'a' and password == 'a':
-                    return redirect(url_for('agregar'))
-                else:
-                    session['error'] = "Usuario o contraseña incorrectos"
-                    return redirect(url_for('index'))  
-            return render_template('index.html', error=error)
 
-        @self.app.route('/agregar', methods=['GET', 'POST'])
-        def agregar():
-            if request.method == 'POST':
-                tipo = request.form.get('tipo')
-                marca = request.form.get('marca')
-                modelo = request.form.get('modelo')
-                precio = request.form.get('precio')
-                cantidad = request.form.get('cantidad')
-                descripcion = request.form.get('descripcion')
-                urlImagen = request.form.get('urlImagen')
+app.secret_key = 'clave'
 
-                # Verificar si algún campo está vacío
-                if not tipo or not marca or not modelo or not precio or not cantidad or not descripcion or not urlImagen:
-                    session['error'] = "Todos los campos son obligatorios"
-                    return redirect(url_for('agregar'))
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    error = None
+    if 'error' in session:
+        error = session['error']
+        session.pop('error', None)
 
-                nuevo_auto = Auto(tipo, marca, modelo, descripcion, precio, cantidad, urlImagen)
-                Auto.agregarAuto(nuevo_auto)
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == 'empleado' and password == '$uper4utos#':
+            return redirect(url_for('agregar'))
+        else:
+            session['error'] = "Usuario o contraseña incorrectos"
+            return redirect(url_for('index'))  
+    return render_template('index.html', error=error)
 
-                # Agregar un mensaje de éxito a la sesión
-                session['success'] = f"Se ha registrado el auto con el idTipo: {tipo} correctamente"
+@app.route('/agregar', methods=['GET', 'POST'])
+def agregar():
+    error = None
+    success = None
+    if request.method == 'POST':
+        tipo = request.form.get('tipo')
+        marca = request.form.get('marca')
+        modelo = request.form.get('modelo')
+        precio = request.form.get('precio')
+        cantidad = request.form.get('cantidad')
+        descripcion = request.form.get('descripcion')
+        urlImagen = request.form.get('urlImagen')
 
-            error = None
-            if 'error' in session:
-                error = session['error']
-                session.pop('error', None)
+        # Verificar si ya existe un auto con el mismo idTipoAuto
+        if any(auto.tipo == tipo for auto in Auto.obtenerAutos()):
+            error = 'Ya existe un auto con el mismo idTipoAuto.'
+            session['error'] = error
+        else:
+            Auto.agregarAuto(tipo, marca, modelo, descripcion, precio, cantidad, urlImagen)
+            success = f"Se ha registrado el auto con el idTipo: {tipo} correctamente"
+            session['success'] = success
 
-            success = None
-            if 'success' in session:
-                success = session['success']
-                session.pop('success', None)
+    if 'error' in session:
+        error = session['error']
+        session.pop('error', None)
 
-            return render_template('agregar.html', error=error, success=success)
+    if 'success' in session:
+        success = session['success']
+        session.pop('success', None)
 
-        @self.app.route('/info')
-        def info():
-            return render_template('info.html')
+    return render_template('agregar.html', error=error, success=success)
 
-        @self.app.route('/registro')
-        def registro():
-            return render_template('registro.html')
+@app.route('/info')        
+def info():
+    return render_template('info.html')
 
-    def run(self):
-        self.app.run(host='localhost', debug=True)
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    if request.method == 'POST':
+        idTipo_eliminar = request.form.get('eliminar')
+        if idTipo_eliminar:
+            Auto.eliminarAuto(idTipo_eliminar)
+    autos = Auto.obtenerAutos()
+    return render_template('registro.html', autos=autos)
 
 if __name__ == '__main__':
-    MyApp().run()
+    app.run(debug=True)
+
+        
